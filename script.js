d551472e9a00529a56b38adc0f4058b43c8a74fa
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-08-30 16:52:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstWord(s) {
   // your code here
+	s = s.trim();
+	let firstSpace = s.indexOf(' '); 
+	if(firstSpace === -1) {
+		return s;
+	};
+
+	return s.substring(0, firstSpace);
 }
 
 // Do not change the code below
